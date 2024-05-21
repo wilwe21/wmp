5e5842d88356ab1592cc1fa3a8e5c8f405313b57
wilwe <wilwe036@gmail.com>
2024-05-21 19:26:13 +0200

    under development info

--- src/modules/fi.js
+++ src/modules/fi.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
-import {getLyrics, getSong } from 'genius-lyrics-api';
 
+function Um({ isUnderDevelopment}) {
+	if (isUnderDevelopment) {
+		return (
+			<h1>
+				Don't Work cus under development
+			</h1>
+		);
+	};
+	return null;
+};
 function FileInput() {
+	const [sus, setSus] = useState(false);
 	const [selectedFile, setSelectedFile] = useState(null);
 
-	/*const handleFileChange = (event) => {
+	const handleFileChange = (event) => {
 		for (let f = 0; f < event.target.files.length; f++) {
 			const file = event.target.files[f]
 			if (!file || !file.type.startsWith('audio/')) {
@@ -32,20 +42,22 @@ function FileInput() {
 				pr.appendChild(nfile);
 				document.body.appendChild(pr);
 			};
-	};*/
-	const handleFileChange = (event) => {
+	};
+	const UrlLoad = (event) => {
 		if (event.key === 'Enter') {
 				const art = document.getElementById("Artist").value;
 				const tit = document.getElementById("Title").value;
 				const key = document.getElementById("apiKey").value;
+				setSus(true);
 		};
 	};
 	return (
 		<div>
 			<input type="file" multiple accept="audio/*" onChange={handleFileChange} />
-			<input type="text" id="Artist" placeholder="Artist" onKeyPress={handleFileChange} />
-			<input type="text" id="Title" placeholder="Title" onKeyPress={handleFileChange} />
-			<input type="text" id="apiKey" placeholder="Api Key" onKeyPress={handleFileChange} />
+			<input type="text" id="Artist" placeholder="Artist" onKeyPress={UrlLoad} />
+			<input type="text" id="Title" placeholder="Title" onKeyPress={UrlLoad} />
+			<input type="text" id="apiKey" placeholder="Api Key" onKeyPress={UrlLoad} />
+			<Um isUnderDevelopment={sus} />
 		</div>
 	);
 }
