b2091a86b469af031768861abdf225bfee9f04fe
wilwe <wilwe036@gmail.com>
2024-09-11 20:39:32 +0200

    work on one file

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,10 @@ fn exline(text: &str, delimiter: &str) -> String {
     }
 }
 
-fn on_active(app: &gtk::Application) {
-    let mainBox = gtk::Box::new(gtk::Orientation::Vertical, 1);
+fn load_file(file: &str) -> gtk::Box {
+    let streambox = gtk::Box::new(gtk::Orientation::Vertical, 1);
     let output = Command::new("ffprobe")
-        .arg("/home/wilwe/Muzyka/Korzenie/Korzenie.mp3")
+        .arg(&file)
         .output()
         .expect("failed to execute");
     let probe  = match str::from_utf8(&output.stderr){
@@ -29,6 +29,9 @@ fn on_active(app: &gtk::Application) {
     };
     let find = ["artist          : ", "title           : ", "album           : ", "track           : ", "TRACKTOTAL      : ", "encoder         : ", "Duration: "];
     let meta = [&exline(&probe, &find[0])[find[0].len()..], &exline(&probe, &find[1])[find[1].len()..], &exline(&probe, &find[2])[find[2].len()..], &exline(&probe, &find[3])[find[3].len()..], &exline(&probe, &find[4])[find[4].len()..], &exline(&probe, &find[5])[find[5].len()..], &exline(&probe, &find[6])[find[6].len()..]];
+    let Stream = gtk::Video::for_filename(Some(&file));
+
+    streambox.append(&Stream);
     let ArtistLab = gtk::Label::builder()
         .label(meta[0])
         .build();
@@ -47,18 +50,38 @@ fn on_active(app: &gtk::Application) {
     let durLab = gtk::Label::builder()
         .label(meta[6])
         .build();
-    mainBox.append(&ArtistLab);
-    mainBox.append(&TitleLab);
-    mainBox.append(&AlbumLab);
-    mainBox.append(&TrackLab);
-    mainBox.append(&EncodecLab);
-    mainBox.append(&durLab);
+    streambox.append(&ArtistLab);
+    streambox.append(&TitleLab);
+    streambox.append(&AlbumLab);
+    streambox.append(&TrackLab);
+    streambox.append(&EncodecLab);
+    streambox.append(&durLab);
     ArtistLab.add_css_class("artist");
     TitleLab.add_css_class("title");
     AlbumLab.add_css_class("album");
     TrackLab.add_css_class("track");
     EncodecLab.add_css_class("encodec");
     durLab.add_css_class("dur");
+    Stream.add_css_class("Stream");
+    streambox
+}
+
+fn on_active(app: &gtk::Application) {
+    let mainBox = gtk::Box::new(gtk::Orientation::Vertical, 1);
+    let ls = Command::new("ls")
+        .arg("/home/wilwe/Muzyka")
+        .output()
+        .expect("not a dir");
+    let dir = match str::from_utf8(&ls.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("that's a wrong number: {}", e)
+    };
+    let file = "/home/wilwe/Muzyka/Korzenie/Korzenie.mp3";
+    let s = load_file(&file);
+    let lab = gtk::Label::builder()
+        .label(dir)
+        .build();
+    mainBox.append(&s);
     let window = gtk::ApplicationWindow::builder()
         .title("WMP")
         .application(app)
