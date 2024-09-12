6f75bc6e6605e04d4690e236dc47aa49dfaf559d
wilwe <wilwe036@gmail.com>
2024-09-12 16:01:25 +0200

    support for file chooser grindset

--- src/main.rs
+++ src/main.rs
@@ -25,43 +25,45 @@ fn load_file(file: &str) -> gtk::Box {
         .expect("failed to execute");
     let probe  = match str::from_utf8(&output.stderr){
         Ok(v) => v,
-        Err(e) => panic!("invalid UTF-8 sequence: {}", e)
+        Err(_) => &"error i chuj"
     };
-    let find = ["artist          : ", "title           : ", "album           : ", "track           : ", "TRACKTOTAL      : ", "encoder         : ", "Duration: "];
-    let meta = [&exline(&probe, &find[0])[find[0].len()..], &exline(&probe, &find[1])[find[1].len()..], &exline(&probe, &find[2])[find[2].len()..], &exline(&probe, &find[3])[find[3].len()..], &exline(&probe, &find[4])[find[4].len()..], &exline(&probe, &find[5])[find[5].len()..], &exline(&probe, &find[6])[find[6].len()..]];
+    if probe != "error i chuj" {
+        let find = ["artist          : ", "title           : ", "album           : ", "track           : ", "TRACKTOTAL      : ", "encoder         : ", "Duration: "];
+        let meta = [&exline(&probe, &find[0])[find[0].len()..], &exline(&probe, &find[1])[find[1].len()..], &exline(&probe, &find[2])[find[2].len()..], &exline(&probe, &find[3])[find[3].len()..], &exline(&probe, &find[4])[find[4].len()..], &exline(&probe, &find[5])[find[5].len()..], &exline(&probe, &find[6])[find[6].len()..]];
+        let ArtistLab = gtk::Label::builder()
+            .label(meta[0])
+            .build();
+        let TitleLab = gtk::Label::builder()
+            .label(meta[1])
+            .build();
+        let AlbumLab = gtk::Label::builder()
+            .label(meta[2])
+            .build();
+        let TrackLab = gtk::Label::builder()
+            .label(format!("{}/{}", meta[3], meta[4]))
+            .build();
+        let EncodecLab = gtk::Label::builder()
+            .label(meta[5])
+            .build();
+        let durLab = gtk::Label::builder()
+            .label(meta[6])
+            .build();
+        streambox.append(&ArtistLab);
+        streambox.append(&TitleLab);
+        streambox.append(&AlbumLab);
+        streambox.append(&TrackLab);
+        streambox.append(&EncodecLab);
+        streambox.append(&durLab);
+        ArtistLab.add_css_class("artist");
+        TitleLab.add_css_class("title");
+        AlbumLab.add_css_class("album");
+        TrackLab.add_css_class("track");
+        EncodecLab.add_css_class("encodec");
+        durLab.add_css_class("dur");
+    }
     let Stream = gtk::Video::for_filename(Some(&file));
 
     streambox.append(&Stream);
-    let ArtistLab = gtk::Label::builder()
-        .label(meta[0])
-        .build();
-    let TitleLab = gtk::Label::builder()
-        .label(meta[1])
-        .build();
-    let AlbumLab = gtk::Label::builder()
-        .label(meta[2])
-        .build();
-    let TrackLab = gtk::Label::builder()
-        .label(format!("{}/{}", meta[3], meta[4]))
-        .build();
-    let EncodecLab = gtk::Label::builder()
-        .label(meta[5])
-        .build();
-    let durLab = gtk::Label::builder()
-        .label(meta[6])
-        .build();
-    streambox.append(&ArtistLab);
-    streambox.append(&TitleLab);
-    streambox.append(&AlbumLab);
-    streambox.append(&TrackLab);
-    streambox.append(&EncodecLab);
-    streambox.append(&durLab);
-    ArtistLab.add_css_class("artist");
-    TitleLab.add_css_class("title");
-    AlbumLab.add_css_class("album");
-    TrackLab.add_css_class("track");
-    EncodecLab.add_css_class("encodec");
-    durLab.add_css_class("dur");
     Stream.add_css_class("Stream");
     streambox
 }
@@ -76,6 +78,21 @@ fn on_active(app: &gtk::Application) {
         Ok(v) => v,
         Err(e) => panic!("that's a wrong number: {}", e)
     };
+    let dialog = gtk::FileChooserDialog::new(
+        None,
+        None/* parent */,
+        gtk::FileChooserAction::Open,
+        &[("Open", gtk::ResponseType::Accept), ("Cancel", gtk::ResponseType::Cancel)],
+        );
+    dialog.show();
+    dialog.connect_response(|dialog, response_type| {
+        if response_type == gtk::ResponseType::Accept {
+            println!("Accept");
+        } else if response_type == gtk::ResponseType::DeleteEvent {
+            println!("Cancel");
+        }
+        dialog.destroy();
+    });
     let file = "/home/wilwe/Muzyka/Korzenie/Korzenie.mp3";
     let s = load_file(&file);
     let lab = gtk::Label::builder()
